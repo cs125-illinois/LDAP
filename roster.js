8e78585e8db72d2f7f2bfeb5f5d0e4bb5cf7626b
Geoffrey Challen <challen@illinois.edu>
2018-09-08 11:52:22 -0500

    Retrieve as much user data as possible. (Not much.)

--- roster.js
+++ roster.js
@@ -2,65 +2,56 @@
 
 'use strict'
 
+const _ = require('lodash')
+
+const chai = require('chai')
+const expect = chai.expect
+chai.use(require('dirty-chai'))
+
 require('dotenv').config()
-const ldapjs = require('ldapjs')
-const bluebird = require('bluebird')
+const LDAP = require('promised-ldap')
 
-const baseDN = "OU=Classes,OU=UsersAndGroups,OU=Engineering,OU=Urbana,DC=AD,DC=UILLINOIS,DC=EDU"
-const tlsOptions = {
-  rejectUnauthorized: false
-}
+const classesDN = "OU=Classes,OU=UsersAndGroups,OU=Engineering,OU=Urbana,DC=AD,DC=UILLINOIS,DC=EDU"
+const peopleDN = "OU=People,DC=AD,DC=UILLINOIS,DC=EDU"
 
-const client = ldapjs.createClient({ url: 'ldap://ad.uillinois.edu/', tlsOptions })
-//bluebird.promisifyAll(client)
+const client = new LDAP({ url: 'ldap://ad.uillinois.edu/' })
+client.starttls({}, []).then(async () => {
+  const login = await client.bind(process.env.ADUSER, process.env.ADPASSWORD)
+  const results = await client.search(classesDN, {
+    filter: `(samaccountname=${ process.env.ADGROUP })`, scope: 'sub'
+  })
+  expect(results.entries.length).to.equal(1)
 
-client.starttls(tlsOptions, [], () => {
-  console.log("Here")
-})
-/*
-client.bind(process.env.USER, process.env.PASSWORD).then(() => {
-  console.log("Here")
-})
+  const netIDs = _(results.entries[0].object.member).map(member => {
+    const [ unused, netID, group ] = /^CN=(.+?),OU=(.+?),/.exec(member)
+    return group === 'People' ? netID : null
+  }).filter(member => {
+    return member !== null
+  }).value()
 
-/*
-client.startttls(
-client.starttls(tlsOptions,[],function(err) {
-    if (err) throw err;
-    client.bind(login, passw, function(err) {
-        if (err) throw err;
+  expect(netIDs.length).to.be.at.least(1)
 
-        var searchOptions = {
-            filter: `(samaccountname=${group})`,
-            scope: 'sub',
-        }
+  let students = {}
+  for (let netID of netIDs) {
+    const results = await client.search(peopleDN, {
+      filter: `(cn=${ netID })`, scope: 'sub'
+    })
+    expect(results.entries.length).to.equal(1)
+    const { givenName: first, sn: last, mail: email } = results.entries[0].object
+    expect(students).to.not.have.property(email)
+    students[email] = {
+      email,
+      name: {
+        first, last, full: `${ first } ${ last }`
+      }
+    }
+  }
+  expect(_.keys(students).length).to.equal(netIDs.length)
 
-        client.search(baseDN, searchOptions, function(err, result) {
-            if (err) throw Error(err);
-
-            result.on('searchEntry', function(entry) {
-                //console.dir(entry.objectName);
-                //console.dir(entry.attributes);
-                if ("member" in entry.object) {
-                    entry.object.member.forEach(function(member) {
-                        printCN(member);
-                    });
-                }
-            });
-
-            result.on('error', function(err) {
-                console.error('error: ' + err.message);
-            });
-
-            result.on('end', result => {
-                //console.log(result.status);
-                client.unbind();
-            });
-        });
-    });
+  console.log(JSON.stringify(students, null, 2))
+  client.unbind()
 })
 
-function printCN(dn) {
-    var res = dn.match(/^CN=(.+?),OU/);
-    console.log(res[1]);
-}
-*/
+process.on('unhandledRejection', (reason, promise) => {
+  console.log(reason.stack || reason)
+})
